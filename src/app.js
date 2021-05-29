8e51493d30f16524339fd517dfebd10771abfdf9
Jesusppy <77135046+Jesusppy@users.noreply.github.com>
2021-05-29 12:22:04 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const passport = require('passport');
 
 
 
 const app = express();
+require('./config/passport');
 
 // Connecting to db
 
@@ -39,8 +42,20 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Global Variables
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //routes
 
