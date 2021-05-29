dfc0e0209318e914b70b8ae6a4c91670e30e3855
Jesusppy <77135046+Jesusppy@users.noreply.github.com>
2021-05-29 12:21:26 -0400

    Add files via upload

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,31 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/Note');
+
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note');
 });
 
-router.post('/notes/new-note', (req, res) => {
-    const { title, description} = req.body;
+router.post('/notes/new-note', async (req, res) => {
+    console.log(req.body)
+    const { title, description } = req.body;
     const errors = [];
     if (!title) {
-        errors.push({text : 'Please write a title'});
+        errors.push( {text: ' Please write a title'});
     }
     if (!description) {
-        errors.push({text: 'Please write a description'});
+        errors.push( {text: 'Please write a description'});
     }
     if (errors.length > 0) {
         res.render('notes/new-note', {
             errors,
             title,
-            description,
+            description
         });
+        console.log(errors) 
+    }else {
+        const newNote = new Note({ title, description});
+        await newNote.save();
+        req.flash('success_msg', 'Note added successfully');
+        res.redirect('/notes');
     }
 });
- 
 
-router.get('/notes', (req, res) => {
-    res.send('Notes from database');
+router.get('/notes', async (req, res) => {
+        const notes = await Note.find().sort({date: 'desc'});                 
+        res.render('notes/all-notes', {
+            notes
+        });
+});
+
+router.get('/notes/edit/:id', async(req,res) => {
+    const note = await Note.findById(req.params.id);
+    res.render('notes/edit-note', {note
+    });
+});
+
+router.put('/notes/edit-note/:id', async(req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description});
+    req.flash('success_msg', 'Note updated successfully');
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async(req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note delected successfully');
+    res.redirect('/notes');
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,21 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users/register', (req,res) => {
-    res.render('users/signup');
+const User = require('../models/User');
+
+const passport = require('passport');
+
+router.get('/users/signin', (req,res) => {
+    res.render('users/signin');
 });
 
-router.post('/users/signup', (req,res) => {
-    console.log(req.body)
-    req.session.my_variable=' Hello world';
-    res.redirect('profile');
+router.post('/users/signin', passport.authenticate('local', {
+    successRedirect: '/notes',
+    failureRedirect: '/users/signin',
+    failureflash: true
+}));
+
+router.get('/users/signup', (req, res) => {  
+    res.render('users/signup')
 });
 
-router.get('/users/profile', (req, res) => {
-    const text = req.session.my_variable;
-    delete req.session.my_variable;
-    res.render('users/profile', {
-        text
-    });
-})
+router.post('/users/signup', async (req,res) => {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+    if (name.length <= 0) {
+        errors.push({ text: 'Please insert your name'});
+    }
+    if (password != confirm_password) {
+        errors.push({ text: 'Password do not match'});
+   }
+    if (password.length < 4 ) {
+        errors.push({ text: 'Password must be at least 4 characters'});
+    }
+    if (errors.length > 0 ) {
+        res.render('users/signup', {
+            errors,
+            name,
+            email,
+            password,
+            confirm_password
+        });
+        console.log(errors)
+     } else {
+         const emailUser = await User.findOne({email: email});
+         if (emailUser) {
+             req.flash('error_msg', 'Email is already in use');
+             res.redirect('/users/signup');
+         }
+            const newUser = new User({
+                name,
+                email, 
+                password
+            });
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', ' You are registered');
+            res.redirect('/users/signin');
+        }
+  });
+
 module.exports = router;
